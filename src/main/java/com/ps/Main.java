f98907c787dea83b127de7abee4ddc71a537bc32
TsareJ <tsarejoseph@gmail.com>
2025-05-02 01:38:29 -0400

    creating methods and adding menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -9,13 +9,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-
-
+    private static Scanner scanner = new Scanner(System.in);
+    private static String string = "transactions.csv";
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public static void main(String[] args) {
+        createFileIfNotExists();
+        homeScreen();
+    }
 
+    private static void createFileIfNotExists() {
+        File
+    }
 
+    private static void homeScreen() {
     }
 
 }
+
